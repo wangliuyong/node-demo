24f7e8ccc18ce54f49178b5cb49725196969e5b1
wangliuyong <1355498705@qq.com>
2018-08-09 14:00:45 +0800

    修改serve

--- serve.js
+++ serve.js
@@ -20,25 +20,16 @@ var server = http.createServer(function(request, response){
   /******** 从这里开始看，上面不要看 ************/
 
   console.log('HTTP 路径为\n' + path)
-  if(path == '/style.js'){
-    response.setHeader('Content-Type', 'text/css; charset=utf-8')
-    response.write('body{background-color: #ddd;}h1{color: red;}')
+  if(path === '/'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write('哈哈哈')
     response.end()
-  }else if(path == '/script.html'){
-    response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
-    response.write('alert("这是JS执行的")')
-    response.end()
-  }else if(path == '/index.css'){
-    response.setHeader('Content-Type', 'text/html; charset=utf-8')
-    response.write('<!DOCTYPE>\n<html>'  + 
-      '<head><link rel="stylesheet" href="/style.js">' +
-      '</head><body>'  +
-      '<h1>你好</h1>' +
-      '<script src="/script.html"></script>' +
-      '</body></html>')
-    response.end()
+  
   }else{
     response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write('呜呜呜')
     response.end()
   }
 
